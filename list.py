4208a26df73af191cc882e094ffb717de3ba8126
Ishwari Shamrao Sawant <ishwarisawant2907@gmail.com>
2026-09-03 03:26:27 +0530

    Update list.py
    Added practice questions and examples for list operations.

--- list.py
+++ list.py
@@ -14,6 +14,7 @@ print(marks[:3])   # access elements from the beginning to index 2
 print(marks[4:])  # access elements from index 1 to the end
 print(marks[::2]) # access every 2nd element from the list
 
+
 #list Methods
 list = [1, 2, 3, 4, 5]
 list.append(60)  # add an element to the end of the list
@@ -45,3 +46,43 @@ print(list)
 
 list.clear()  # remove all elements from the list
 print(list)
+
+
+#Practice Questions
+# 1. WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
+movie = []
+mov_1 = input("enter first movie name ")
+mov_2 = input("enter second movie name ")
+mov_3 = input("enter third movie name ")
+
+movie.append(mov_1)
+movie.append(mov_2)
+movie.append(mov_3)
+print(movie)
+
+
+#2. WAP to check if a list contains a palindrome of elements. (Hint: use copy( ) method)
+
+list1 = ["m","a","d","a","m"]
+list2 = ["l","e","a","f"]
+
+copy_list1 = list1.copy()
+copy_list1.reverse()
+
+if (copy_list1 == list1):
+    print("palindrome")
+else:
+    print("not a palindrome")
+
+copy_list2 = list2.copy()
+copy_list2.reverse()
+if (copy_list2 == list2):
+    print("palindrome")
+else:
+    print("not a palindrome")
+
+
+#3. Store the above values in a list & sort them from "A" to "D".
+grades = ["C", "D", "A", "A", "B", "B", "A"]
+grades.sort()
+print(grades)
